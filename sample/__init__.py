be311c83524358630bdb2d1f438a8e72f2627a61
Pirot1 <122035339+Pirot1@users.noreply.github.com>
2023-10-23 20:20:07 +0600

    Revert "Pirot1 patch 1 (#1)"
    This reverts commit e32bbceb4d20696e7b6bbb729437356bd8a06f45.

--- sample/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Sample Package
-"""
-
-from .cli import cli
--- sample/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Main module for debugging
-"""
-
-from .cli import cli
-
-
-if __name__ == "__main__":
-    cli()  # pylint: disable=no-value-for-parameter
